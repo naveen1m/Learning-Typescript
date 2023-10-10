f7c3d64b2774c91f4289976355fb5e8daaba084a
naveen <stupiddint@gmail.com>
2023-10-10 12:33:18 +0530

    build folder in js to run ts

---
+++ Node-TS/dist/app.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello, Learnig to use ts in node and using concurrently package to build and run at the same time.");
+});
+app.listen(5000, () => {
+    console.log(`server is running at http://localhost:5000`);
+});
