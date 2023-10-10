c5a506a9e788c01eaac1f5fc16ec59b7f4fd8ab2
naveen <stupiddint@gmail.com>
2023-10-10 12:33:53 +0530

    node setup and running ts code

--- Node-TS/src/app.ts
+++ Node-TS/src/app.ts
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello, Learnig to use ts in node and using concurrently package to build and run at the same time.")
+})
+
 app.listen(5000, () => {
     console.log(`server is running at http://localhost:5000`)
 })
